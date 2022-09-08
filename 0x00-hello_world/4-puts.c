29d71a46c8e9ff3a1400857f17ac44e6d25759c8
AngelaChebet <angela.chebet@googlemail.com>
2022-09-08 11:27:37 -0700

    add 4-puts.c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
+/**
+ * MyString to print out programming is like building a multilingual puzzle
+ */
 int main(void)
 {
-	char MyString[] = "Programming is like building a multilingual puzzle";
+	char MyString[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(MyString);
 
