f38d8a00ad0611693583f6c243e4e797204d4060
AngelaChebet <angela.chebet@googlemail.com>
2022-09-09 14:18:36 -0700

    add 5-print_numbers.c file

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digits in base 10
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1 ; i < 10 ; i++)
+{
+		printf("%d", i);
+}
+		putchar('\n');
+
+	return (0);
+}
