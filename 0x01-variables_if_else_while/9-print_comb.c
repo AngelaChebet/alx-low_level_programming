d58aa3619c0b31e7e282e1224cc0d6f826eb89bf
AngelaChebet <angela.chebet@googlemail.com>
2022-09-10 23:55:14 -0700

    add 9-print_comb.c file

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-/** 
+/**
  * main -print single digit numbers separated by a comma and in ascending order
  *
  * Return: Always 0 (Success)
@@ -9,10 +9,11 @@ int main(void)
 {
 	int i = 0;
 
-	while (i <10)
+	while (i < 10)
 {
 	putchar((i % 10) + '0');
 	putchar(',');
+	putchar(' ');
 
 	i++;
 }
