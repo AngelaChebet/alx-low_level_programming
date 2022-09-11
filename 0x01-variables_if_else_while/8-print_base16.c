2897a5a99376424d9f99dd6d80b4f475111a1e35
AngelaChebet <angela.chebet@googlemail.com>
2022-09-10 23:08:22 -0700

    add 8-print_base16.c file

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -3,7 +3,7 @@
 /**
  * main -print all the numbers in base 16 in lowercase
  *
- * Base 16 uses powers of 16. The possible digits are 0, 1, 2, 3, 4, 5, 6, 
+ * Base 16 uses powers of 16. The possible digits are 0, 1, 2, 3, 4, 5, 6,
  * 7, 8, 9, A, B, C, D, E, and F
  *
  * Return: Always 0 (Success)
