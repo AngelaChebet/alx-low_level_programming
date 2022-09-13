632c39ec2e7f8dc3ab3fd52721b9961c95bf5590
AngelaChebet <angela.chebet@googlemail.com>
2022-09-13 12:44:40 -0700

    add 3-islower.c file

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * _islower -checks for an alphabet that has lower case 
+ * int _islower(int c) checks for lower case 
  * and brings back 1 if lowercase and 0 if not
  *
- * return: 1 or 0 (Success)
+ * return value: 1 or 0 (Success)
  */
 
 int _islower(int c)
 {
-	if (c >= 'a' && c<= 'z')
+	if (c >= 'a' && c <= 'z')
 {
 	return (1);
 }
