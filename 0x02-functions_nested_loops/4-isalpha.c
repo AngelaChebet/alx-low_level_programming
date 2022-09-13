aa04eddeb88b010013654936580f42a7b3c17001
AngelaChebet <angela.chebet@googlemail.com>
2022-09-13 13:24:02 -0700

    add 4-isalpha.c file

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,13 +3,13 @@
 /**
  * _isalpha -checks if character c is alphabet
  *
- * @c -the character to be checked
+ * @c -the parameter c to be checked
  *
- * return: 1 if character is a letter otherwise return 0
+ * return value for _isalpha: 1 if c is a letter otherwise return 0
  */
 int _isalpha(int c)
 {
-	if ((c >= 'a'&& c <= 'z') || (c >= 'A' && c <= 'Z'))
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 {
 	return (1);
 }
