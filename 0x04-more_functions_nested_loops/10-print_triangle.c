a38fe1e7e015c5817acba4a0bcadd402486f04df
AngelaChebet <angela.chebet@googlemail.com>
2022-09-15 13:49:52 -0700

    add changes to file

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -14,16 +14,16 @@ void print_triangle(int size)
 
 	if (size > 0)
 	{
-	for (hash = 1; hash <= size; hash++)
-	{
-	for (index = size - hash; index > 0; index--)
-											_putchar(' ');
-	for (index = 0; index < hash; index++)
-											_putchar('#');
-	if (hash == size)
-											continue;
-											_putchar('\n');
-											}
+		for (hash = 1; hash <= size; hash++)
+		{
+			for (index = size - hash; index > 0; index--)
+				_putchar(' ');
+			for (index = 0; index < hash; index++)
+				_putchar('#');
+			if (hash == size)
+				continue;
+			_putchar('\n');
+		}
 	}
-		_putchar('\n');
+	_putchar('\n');
 }
