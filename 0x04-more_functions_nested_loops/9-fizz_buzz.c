f05a904e42d34bda0c9b4e70c40d720e05ced22f
AngelaChebet <angela.chebet@googlemail.com>
2022-09-15 12:53:05 -0700

    add changes

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 /**
- * main -prints numbers 1 to 100, multiple of 3 is
- * printed Fizz, multiple of 5 is printed Buzz and multiple
- * of both 3 and 5 is printed FizzBuzz
+ * main -display numbers 0 to 100, numbers multiple of 3 are
+ * replaced with the word Fizz, numbers multiple of 5 are
+ * replaced with the word Buzz, numbers that are multiples of 3
+ * and 5 are replaced with the word FizzBuzz
  *
- * Return: always 0 (Success)
+ * Return: always 0 (success)
  */
 int main(void)
 {
@@ -15,12 +16,14 @@ int main(void)
 	{
 		if ((n % 3) == 0 && (n % 5) == 0)
 		{
-			printf("FizzBuzz");
+			printf("Fizzbuzz");
 		}
+
 		else if ((n % 3) == 0)
 		{
 			printf("Fizz");
 		}
+
 		else if ((n % 5) == 0)
 		{
 			printf("Buzz");
